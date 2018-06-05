48491551106d3c43c1da1ad6e9ef80982d6dbabd
Michel Gouzée <michel.gouzee@hotmail.com>
2018-06-05 06:52:24 +0200

    First Commit with content

---
+++ Crawler/Program.cs
@@ -0,0 +1,24 @@
+﻿using CsQuery;
+using System;
+using System.Net.Http;
+
+namespace Test_Console
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string baseUrl = "https://rpg.rem.uz/";
+
+            var dom = CQ.CreateFromUrl(baseUrl);
+
+            string htmlTarget = dom["#view"].Render();
+
+            Console.WriteLine(htmlTarget);
+
+            Console.ReadLine();
+        }
+    }
+
+}
