752116cd81149526423bee5d126b93bb5ac9f59c
Michel Gouzee <mgouzee@interparking.com>
2018-06-05 14:20:25 +0200

    Functional version

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -2,7 +2,9 @@
 using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 
 namespace Test_Console
@@ -10,98 +12,59 @@ namespace Test_Console
     class Program
     {
         public static List<string> endTargets = new List<string>();
+        public static HtmlWeb web = new HtmlWeb();
+        const string baseUrl = "https://rpg.rem.uz";
+
+        const string folderToSave = "YOUR FOLDER";
 
         static void Main(string[] args)
         {
-            string baseUrl = "https://rpg.rem.uz";
-          
-            //string urlModified = baseUrl + "/WOIN/";
-
-            var web = new HtmlWeb();
-            var doc = web.Load(baseUrl);
-            var firstHtml = doc.DocumentNode.InnerHtml;
-            //var docModified = web.Load(urlModified);
-
-            //bool isSamePage = (doc.DocumentNode.InnerHtml == docModified.DocumentNode.InnerHtml);
-
-            //Console.WriteLine(isSamePage);
-
-            var target = doc.DocumentNode.SelectNodes("//td/a");
-
             var BuildPath = new List<string>();
-            BuildPath = ExtractPath(target, BuildPath);
+                        
+            var doc = web.Load(baseUrl);
 
-            foreach (var item in BuildPath)
-            {
-                string page = baseUrl + item;
-                doc = web.Load(page);
+            FilterPath(doc);
 
-                var truc = doc.DocumentNode.InnerHtml;
-                if (truc == firstHtml)
-                {
-                    Console.WriteLine("Same page loaded");
-                    continue;
-                }                
-
-                target = doc.DocumentNode.SelectNodes("//td/a");
-                var newList = ExtractPath(target, BuildPath);
-            }
+            WebClient client = new WebClient();
 
             foreach (var item in endTargets)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(baseUrl + item);
+                //todo : copy file instead of writeline
+                //client.DownloadFile(baseUrl + item, folderToSave);
             }
 
+            Console.WriteLine(endTargets.Count +" files downloaded");
             Console.ReadLine();
 
-            #region httpclient
-            //HttpClient client = new HttpClient();
-
-            //client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (compatible; AcmeInc/1.0)");
-
-            //var response = client.GetAsync(baseUrl).Result;
-
-            //var html = response.Content.ReadAsStringAsync().Result;
-            #endregion
-
-            #region test with CsQuery
-            //Console.WriteLine(html);
-
-            //var dom = CQ.Create(html);
-
-            ////Console.WriteLine(dom.Render());
-
-            //var mainrow = dom["#mainrow"];
-
-            //Console.WriteLine(mainrow);
-
-            //var content = mainrow["#content"];
-
-            //Console.WriteLine(content);
-            #endregion
         }
 
-        public static List<string> ExtractPath(HtmlNodeCollection target, List<string> BuildPath)
+        public static void FilterPath(HtmlDocument doc)
         {
-            List<string> SecondLevels = new List<string>();
+            var target = doc.DocumentNode.SelectNodes("//td/a");
 
             foreach (var item in target)
             {
                 var href = item.Attributes["href"].Value;
-                
-                //Console.WriteLine(item.Attributes["href"].Value);
 
-                if (!IsEndPath(href) && !BuildPath.Contains(href) && (href != ".."))
+                if (href == "..")
+                    continue;
+
+                if (IsEndPath(href))
                 {
-                    SecondLevels.Add(href);
+                    if (!endTargets.Contains(href))
+                    {
+                        endTargets.Add(href);
+                        continue;
+                    }
                 }
 
-                if(IsEndPath(href) && !endTargets.Contains(href))
-                {
-                    endTargets.Add(href);
-                }
+                var uri = baseUrl + href;
+                var newDoc = web.Load(uri);
+
+                FilterPath(newDoc);
+
             }
-            return SecondLevels;
         }
 
         public static bool IsEndPath(string path)
