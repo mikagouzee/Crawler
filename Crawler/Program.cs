53828d5ef0af282bde4fba8c5feb4445b89f51e5
Michel Gouzee <mgouzee@interparking.com>
2018-06-07 08:16:06 +0200

    Replaced webclient by httpclient, fixed timeout issue

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -6,23 +6,26 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Test_Console
 {
     class Program
     {
         public static List<string> endTargets = new List<string>();
+        //public static List<Task> listOfTasks = new List<Task>();
         public static HtmlWeb web = new HtmlWeb();
         const string baseUrl = "https://rpg.rem.uz";
 
         public static string folderToSave = "YOUR FOLDER";
 
-        public static WebClient client = new WebClient();
+        public static HttpClient client = new HttpClient();
 
         static void Main(string[] args)
         {
             var doc = web.Load(baseUrl);
 
+            client.Timeout = TimeSpan.FromMinutes(15);
             //var testValue = @"/13th%20Age/13th%20Age%20Monthly%20-%20Vol%201/01%20-%20Dragon%20Riding.pdf";
 
             FilterPath(doc);
@@ -30,12 +33,21 @@ namespace Test_Console
 
             foreach (var item in endTargets)
             {
-                //todo : copy file instead of writeline
                 DownloadFile(item);
             }
             
             Console.WriteLine(endTargets.Count +" files downloaded");
             Console.ReadLine();
+
+            #region asyncprep
+            //List<Stream> allStreams = await Task.WhenAll(listOfTasks);
+
+            //foreach (var task in listOfTasks)
+            //{
+
+            //}
+
+            #endregion
 
         }
 
@@ -48,13 +60,36 @@ namespace Test_Console
             var localPath = item.Remove(startindex).Replace('/', '\\');
 
             var fullPath = folderToSave + localPath;
-
-            Console.WriteLine(fullPath);
-
+            
             Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
 
-            client.DownloadFile(baseUrl+item, (fullPath + fileName));
+            Stream downloadedFile = client.GetAsync(baseUrl + item).Result.Content.ReadAsStreamAsync().Result;
+            
+            using (Stream file = File.Create(fileName))
+            {
+                Console.WriteLine("downloading " + fileName);
+                CopyStream(downloadedFile, file);
+            }
+
+            #region async preparation 
+            //Task<Stream> downloadedFile = client.GetAsync(baseUrl + item).Result.Content.ReadAsStreamAsync();
+
+            //listOfTasks.Add(downloadedFile);
+            #endregion
+
         }
+
+        public static void CopyStream(Stream input, Stream output)
+        {
+            byte[] buffer = new byte[8 * 1024];
+
+            int len;
+
+            while((len= input.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                output.Write(buffer, 0, len);
+            }
+         }
 
         public static void FilterPath(HtmlDocument doc)
         {
