4b22ef6cc54a70bd683402cc0ac92234343b5f19
Michel Gouzee <mgouzee@interparking.com>
2018-06-08 09:38:50 +0200

    Make call parallel, created test list, clean unused code (still not working tho)

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -13,11 +13,11 @@ namespace Test_Console
     class Program
     {
         public static List<string> endTargets = new List<string>();
-        public static List<Task<HttpResponseMessage>> listOfTasks = new List<Task<HttpResponseMessage>>();
+        public static List<Task> listOfTasks = new List<Task>();
         public static HtmlWeb web = new HtmlWeb();
         const string baseUrl = "https://rpg.rem.uz";
 
-        public static string folderToSave = "YOUR FOLDER";
+        public static string folderToSave = @"C:\Users\mgouzee\Desktop\Crawler\Crawler\Downloaded";
 
         public static HttpClient client = new HttpClient();
 
@@ -28,41 +28,42 @@ namespace Test_Console
             client.Timeout = TimeSpan.FromMinutes(15);
             //var testValue = @"/13th%20Age/13th%20Age%20Monthly%20-%20Vol%201/01%20-%20Dragon%20Riding.pdf";
 
-            FilterPath(doc);
+            //FilterPath(doc);
 
             string localPath, fullPath;
             int startIndex;
 
+            endTargets = new List<string>();
+            endTargets.Add("/13th%20Age/13%20True%20Ways.pdf");
+            endTargets.Add("/13th%20Age/13th%20Age%20-%20Map.pdf");
+            endTargets.Add("/13th%20Age/13th%20Age%20Bestiary%202%20-%20Lions%20%26%20Tigers%20%26%20Owlbears.pdf");
+
+
             Parallel.ForEach<string>(endTargets, item =>
+            //foreach(var item in endTargets)
             {
-                var fileName = item.Split('/').Last();
-                startIndex = item.IndexOf(fileName);
-                localPath = item.Remove(startIndex).Replace('/', '\\');
-                fullPath = folderToSave + localPath;
-                Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
-                listOfTasks.Add(client.GetAsync(baseUrl + item));
+                var task = client.GetAsync(baseUrl + item)
+                    .ContinueWith(async (x) =>
+                    {
+                        var fileName = item.Split('/').Last();
+                        startIndex = item.IndexOf(fileName);
+                        localPath = item.Remove(startIndex).Replace('/', '\\');
+                        fullPath = folderToSave + localPath;
+                        Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+
+                        var streamDest = File.Create(fileName);
+
+                        await x.Result.Content.CopyToAsync(streamDest);
+
+                    });
+
+                listOfTasks.Add(task);// client.GetAsync(baseUrl + item));
             });
 
-            Task.WhenAny(listOfTasks).ContinueWith((x) =>
-             {
-                 Console.WriteLine("downloading " + fileName);
-                 
-                 x.Result.Content.CopyToAsync(File.Create(fileName));
-             });
+            Task.WaitAll(listOfTasks.ToArray());
 
-
-            Console.WriteLine(endTargets.Count +" files downloaded");
+            Console.WriteLine(endTargets.Count + " files should have been downloaded");
             Console.ReadLine();
-
-            #region asyncprep
-            //Stream[] allStreams = await Task.WhenAll(listOfTasks);
-            
-            //foreach (var stream in allStreams)
-            //{
-            //    using (Stream file = File.Create(stream))
-            //}
-
-            #endregion
 
         }
 
@@ -77,13 +78,13 @@ namespace Test_Console
             var localPath = item.Remove(startindex).Replace('/', '\\');
 
             var fullPath = folderToSave + localPath;
-            
+
             Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
 
             //return Task downloadFile = client.GetAsync(baseUrl + item).Result.Content.CopyToAsync(File.Create(fileName));
 
             //await downloadFile;
-            
+
         }
 
         public static void CopyStream(Stream input, Stream output)
@@ -92,43 +93,54 @@ namespace Test_Console
 
             int len;
 
-            while((len= input.Read(buffer, 0, buffer.Length)) > 0)
+            while ((len = input.Read(buffer, 0, buffer.Length)) > 0)
             {
                 output.Write(buffer, 0, len);
             }
-         }
+        }
 
         public static void FilterPath(HtmlDocument doc)
         {
             var target = doc.DocumentNode.SelectNodes("//td/a");
 
+            var list = new List<Task>();
+
+            if (target == null)
+                return;
+
             foreach (var item in target)
             {
                 var href = item.Attributes["href"].Value;
 
-                if (href == "..")
-                    continue;
-
-                if (IsEndPath(href))
+                if (href != "..")
                 {
-                    if (!endTargets.Contains(href))
+                    if (IsEndPath(href))
                     {
-                        endTargets.Add(href);
-                        continue;
+                        if (!endTargets.Contains(href))
+                        {
+                            endTargets.Add(href);
+                            continue;
+                        }
                     }
+
+                    var uri = baseUrl + href;
+                    //var newDoc = web.Load(uri);
+                    var task = web.LoadFromWebAsync(uri).ContinueWith((x) =>
+                    {
+                        FilterPath(x.Result);
+                    });
+
+                    list.Add(task);
+                    Console.WriteLine(list.Count);
                 }
-
-                var uri = baseUrl + href;
-                var newDoc = web.Load(uri);
-
-                FilterPath(newDoc);
-
             }
+
+            Task.WaitAll(list.ToArray());
         }
 
         public static bool IsEndPath(string path)
         {
-           return !path.EndsWith("/");
+            return !path.EndsWith("/");
         }
 
     }
